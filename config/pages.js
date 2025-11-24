6eb0a94668d55eccfc3eeb126c3e4f209949cc14
PornoBook <pedant-raps.07@icloud.com>
2025-11-24 21:54:59 +0300

    Update pages.js

--- config/pages.js
+++ config/pages.js
@@ -3,6 +3,7 @@ const pages_bott = `
 <a class="page-button" href="index.html">1</a>
 <a class="page-button" href="page_2.html">2</a> 
 <a class="page-button" href="page_3.html">3</a> 
+<a class="page-button" href="page_4.html">4</a> 
 `;
 document.getElementById('paginationtop').innerHTML = pages_bott;
 document.getElementById('paginationfoot').innerHTML = pages_bott;
